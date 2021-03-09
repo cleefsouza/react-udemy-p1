803e1d69fbeb5f10da72f32ea8c89a50e96d6839
Cleef Souza <a.cleef.souza@gmail.com>
2021-03-09 02:04:27 -0300

    Inicio: Lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		posts: [],
+	};
+
+	componentDidMount() {
+		this.loadPosts();
+	}
+
+	loadPosts = async () => {
+		await fetch('https://jsonplaceholder.typicode.com/posts')
+			.then((res) => res.json())
+			.then((posts) => this.setState({ posts }));
+	};
+
+	render() {
+		const { posts } = this.state;
+
+		return (
+			<section className="container">
+				<div className="posts">
+					{posts.map((post) => (
+						<div key={post.id} className="post">
+							<h1>{post.title}</h1>
+							<p>{post.body}</p>
+						</div>
+					))}
+				</div>
+			</section>
+		);
+	}
 }
 
 export default App;
